3e532fec3de1fbe386c42e7b4bd361724d11afa4
José G. Quenum <josum@icloud.com>
2020-07-15 16:48:13 +0200

    some more cleaning and code commenting

--- src/message_buffer.rs
+++ src/message_buffer.rs
@@ -14,11 +14,13 @@ impl<'a> MessageBuffer<'a> {
 		}
 	}
 
+	//add a simple message to the buffer so it can be retrieved when the message is lost 
 	pub fn add_to_buffer(&mut self, msg: &'a messenger::SimpleMessage<'a>) -> Result<&str, &str> {
 		self.buff.push(msg);
 		return Ok("New mesage added")
 	}
 
+	// find a message from the buffer in order to share it wit iots in case of message loss
 	pub fn find_message(&self, message_id: u32) -> Option<&'a messenger::SimpleMessage<'a>> {
 		let msg_iter = self.buff.iter();
 
@@ -29,31 +31,7 @@ impl<'a> MessageBuffer<'a> {
 				continue;
 			}
 		}
-		
+
 		return None
 	}
-} 
-
-// pub fn create_buffer<'a>() -> MessageBuffer<'a> {
-// 	MessageBuffer {
-// 		buff: Vec::new()
-// 	}
-// }
-
-// pub fn add_to_buffer<'a>(mb: &mut MessageBuffer<'a>, msg: &'a messenger::SimpleMessage<'a>) {
-// 	mb.buff.push(msg);
-// }
-
-// pub fn find_message<'a>(mb: &'a MessageBuffer<'a>, message_id: u32) -> Option<&'a messenger::SimpleMessage<'a>> {
-// 	let msg_iter = mb.buff.iter();
-
-// 	for msg in msg_iter {
-// 		if msg.mid == message_id {
-// 			return Some(msg);
-// 		} else {
-// 			continue;
-// 		}
-// 	}
-	
-// 	return None
-// }
+}
